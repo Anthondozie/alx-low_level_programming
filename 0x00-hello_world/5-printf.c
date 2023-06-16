841b24b045878db0fa5e8389d5d7f097bee2f4cf
Anthondozie <mmodozieonyeka@gmail.com>
2023-06-16 06:01:27 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
+
+
+
+After you save the vi file and you’re on webterm you have to compile the file use command below to compile
+
+gcc 5-printf.c -o 5-printf
