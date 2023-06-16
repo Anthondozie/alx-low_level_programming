bc7f246bf735244bee2d6674a7ef1adf1c1f1c45
Anthondozie <mmodozieonyeka@gmail.com>
2023-06-16 06:11:35 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - This is the main function
  *
- * Return: Always 0 (Success)
+ * Return - Always 0
  */
+
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
-
-After you save the vi file and you’re on webterm you have to compile the file use command below to compile
-
-gcc 4-puts.c -o 4-puts
